9db13a2d67d6da108274637564074526aab51740
FourMoBro <FourMoBro@users.noreply.github.com>
2020-12-12 07:45:09 -0800

    added initial components

--- frontend-react/legobase/src/App.js
+++ frontend-react/legobase/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Parts from './components/Parts';
+import PartLoadingComponent from './components/PartLoading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const PartLoading = PartLoadingComponent(Parts);
+	const [appState, setAppState] = useState({
+		loading: false,
+		parts: null,
+	});
 
+	useEffect(() => {
+		setAppState({ loading: true });
+		const apiUrl = `http://127.0.0.1:8000/api/`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((parts) => {
+				setAppState({ loading: false, parts: parts });
+			});
+	}, [setAppState]);
+	return (
+		<div className="App">
+			<h1>Latest Parts</h1>
+			<PartLoading isLoading={appState.loading} parts={appState.parts} />
+		</div>
+	);
+}
 export default App;
