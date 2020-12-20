4a9e92549be6f8a4be1257faf85816b12bd21f63
FourMoBro <FourMoBro@users.noreply.github.com>
2020-12-20 07:05:23 -0800

    updated theme colors

--- frontend-react2/legobase/src/theme/index.js
+++ frontend-react2/legobase/src/theme/index.js
@@ -4,12 +4,21 @@ import typography from './typography';
 
 const theme = createMuiTheme({
   palette: {
-    background: {dark: '#525865', default: colors.common.white, paper: colors.common.white},
-    primary: {main: colors.indigo[500]},
-    secondary: {main: colors.indigo[500]},
-    text: {primary: colors.blueGrey[900], secondary: colors.blueGrey[600]}},
+    background: {dark: colors.blueGrey[700], default: colors.common.white, paper: colors.common.white},
+    primary: {main: colors.blueGrey[900]},
+    secondary: {main: colors.blueGrey[900]},
+    text: {primary: colors.deepOrange[500], secondary: colors.cyan[600]}},
   shadows,
   typography
 });
 
 export default theme;
+
+//good card color #31333b
+//#cce2ff text selection
+//button color? #008cc1
+//#000000de
+
+//#bcc0c9 lables-relationships
+//#fd766e nice orange text
+
