df4483a0adb45b29846488c7a1e26bfa9b7f06a7
FourMoBro <FourMoBro@users.noreply.github.com>
2021-01-07 19:04:50 -0800

    fixed index

--- frontend-react2/legobase/src/index.jsx
+++ frontend-react2/legobase/src/index.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-//import * as serviceWorker from './serviceWorker';
+import { setContext } from '@apollo/client/link/context'
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import App from './App';
+
+
+//Setup Apoll Server GraphQl endpoint URI
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4001/graphql',
+})
+
+// Get the token and the headers from the Django Server/LoginView
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('refresh_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+
+//Use the token for all requests to the GraphQL server
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+       <App />
+    </ApolloProvider>
   </BrowserRouter>
 ), document.getElementById('root'));
 
